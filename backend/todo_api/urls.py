a533176edfc1f2a21f3d5c11732711b3a9a1d4bf
adeesha12 <a3adeeshaanjana@gmail.com>
2024-09-04 13:34:51 +0530

    pytest changes

--- backend/todo_api/urls.py
+++ backend/todo_api/urls.py
@@ -2,5 +2,6 @@ from .views import TodosView
 from django.urls import path
 
 urlpatterns = [
-    path('basic/',TodosView.as_view())
+    path('basic/',TodosView.as_view()),
+    path('basic/<int:id>',TodosView.as_view())
 ]
--- backend/todo_api/views.py
+++ backend/todo_api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,12 +8,17 @@ from .serializer import TodosSerializer
 
 # Create your views here.
 class TodosView(APIView):  
-  
-    def get(self, request, *args, **kwargs):  
+                
+    def get(self,request,id =None): 
+        if id:
+            result = Todos.objects.get(id=id)
+            serializers = TodosSerializer(result)
+            return Response({'status': 'success', "todos":serializers.data}, status=200) 
+        
         result = Todos.objects.all()  
         serializers = TodosSerializer(result, many=True)  
         return Response({'status': 'success', "todos":serializers.data}, status=200)  
-  
+    
     def post(self, request):  
         serializer = TodosSerializer(data=request.data)  
         if serializer.is_valid():  
@@ -21,3 +26,17 @@ class TodosView(APIView):
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)  
         else:  
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)  
+        
+    def patch(self, request, id):
+        result = Todos.objects.get(id=id)
+        serializer = TodosSerializer(result,data= request.data , partial= True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"sucess","data":serializer.data})
+        else:
+            return Response({"status":"error","data":serializer.errors})
+        
+    def delete(self, request, id=None):
+        result = get_object_or_404(Todos, id=id) 
+        result.delete()
+        return Response({"status":"success","data":"Record Delete"})
